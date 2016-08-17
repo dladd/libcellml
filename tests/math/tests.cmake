a385c3580bc65bd8dd41dcf9e3f56a307eed27c4
David Ladd <david.c.ladd@gmail.com>
2016-08-17 15:42:04 +1200

    include symengine in cmake build

--- tests/math/tests.cmake
+++ tests/math/tests.cmake
@@ -11,6 +11,7 @@ list(APPEND LIBCELLML_TESTS ${CURRENT_TEST})
 # Using absolute path relative to this file
 set(${CURRENT_TEST}_SRCS
   ${CMAKE_CURRENT_LIST_DIR}/math.cpp
+  ${CMAKE_CURRENT_LIST_DIR}/symengine.cpp
 )
 set(${CURRENT_TEST}_HDRS
 )
